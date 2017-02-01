8dfa18e77f59c620332231011b1265233cf81cbe
Chinnu M V <chinnu@macmini5.local>
2017-02-01 15:17:27 +0530

    first commit

---
+++ sam/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  sam
+//
+//  Created by Chinnu M V on 2/1/17.
+//  Copyright © 2017 cicitrus.applicationsam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Chinnu")
+        print("sam")
+        print("last")
+        print("second")
+        print("chinnulast")
+        print("secondbbb")
+        print("chinnulast   ")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
